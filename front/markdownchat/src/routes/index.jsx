b0ca49a32273826a0f37f80693f2f1bb9ee177a9
Ivan L. Seibel <me@ivanseibel.dev>
2021-08-08 14:23:28 -0300

    Redirect user if connected

--- front/markdownchat/src/routes/index.jsx
+++ front/markdownchat/src/routes/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { RoomContext } from '../context';
 import { Home } from '../ui/pages';
+import { Room } from '../ui/pages';
 
 const Routes = () => {
+  const { isConnected } = useContext(RoomContext);
+
+  const actualComponent = !isConnected
+    ? Home
+    : Room
+
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Route path="/" exact component={actualComponent} />
     </Switch>
   );
 };
