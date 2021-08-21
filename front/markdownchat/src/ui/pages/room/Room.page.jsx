55bcf444b35143b6b22a55524441c673e1a74a19
Ivan L. Seibel <me@ivanseibel.dev>
2021-08-21 18:01:03 -0300

    Layout improvements

--- front/markdownchat/src/ui/pages/room/Room.page.jsx
+++ front/markdownchat/src/ui/pages/room/Room.page.jsx
@@ -1,14 +1,16 @@
 import React, { useCallback, useState, useRef, useContext, useEffect } from 'react';
-import { format } from 'date-fns';
 import ReactMarkdown from 'react-markdown'
 import { FiLogOut, FiSend, FiMenu } from 'react-icons/fi';
+import emoji from 'emoji-dictionary';
 
 import { RoomContext } from '../../../context';
+import logo from '../../../static/logo.png';
 
 import './styles.css';
 
 export function Room() {
   const [message, setMessage] = useState('');
+  const [showUsers, setShowMenu] = useState(true);
   const {
     isConnected,
     handleSendMessage,
@@ -25,6 +27,15 @@ export function Room() {
     messageContainer.scrollTop = messageContainer.scrollHeight;
   }, [messageHistory]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      const { innerWidth } = window;
+      setShowMenu(innerWidth > 600);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const handleOnChange = useCallback((e) => {
     setMessage(e.target.value);
@@ -42,49 +53,48 @@ export function Room() {
     <div className="chat-room-container">
       <div className="chat-room-header-container">
         <div className="signed-room-div">
+          <img src={logo} alt="logo" className="logo-room" />
           <h3>Room: {signedRoom}</h3>
         </div>
         <div className="leave-room-div">
-          <div className="leave-room-button-container">
-            <input
-              type="button"
-              value="Leave"
-              id="chat-connect"
-              onClick={handleConnect}
-              className="leave-room-button"
-            />
+          <button
+            type="button"
+            id="chat-connect"
+            onClick={handleConnect}
+            className="leave-room-button"
+          >
             <FiLogOut className="logout-icon" size={20} />
-          </div>
+          </button>
         </div>
       </div>
       <div className="message-container">
-        <div className="users-list-container">
+        <div className={showUsers ? 'users-list-container' : 'hide-menu'}>
           <ul className="layout-list">
             {usersList.map((item, index) => (
               <li className="username-list-item" key={index.toString()}>{item.username}</li>
             ))}
           </ul>
         </div>
-        <div className="message-list-container">
+        <div className={showUsers ? "message-list-container" : "message-list-container expand-message-list-container"}>
           <div className="message-list-container-header" >
-            <FiMenu className="menu-icon" size={20} />
+            <button onClick={() => { setShowMenu(state => !state) }} >
+              <FiMenu className="menu-icon" size={20} />
+            </button>
           </div>
           <ul className="layout-list" id="messages-container">
             {messageHistory.map((item, index) => {
               const messageItemClass = item.username === signedUser ? "message-item right" : "message-item";
               const messageBoxClass = item.username === signedUser ? "message-box me" : "message-box";
-              const time = format(new Date(), 'MM/dd/yyyy HH:mm');
+              const emojiSupport = item.message.replace(/:\w+:/gi, name => emoji.getUnicode(name));
 
               return (
                 <li className={messageItemClass} key={index.toString()}>
                   <strong>{item.username}</strong>
                   <div className={messageBoxClass}>
-                    <p>
-                      <ReactMarkdown>
-                        {item.message}
-                      </ReactMarkdown>
-                    </p>
-                    <p className="message-time">{time}</p>
+                    <ReactMarkdown className="markdown-container">
+                      {emojiSupport !== 'undefined' ? emojiSupport : item.message || ''}
+                    </ReactMarkdown>
+                    <p className="message-time">{item.time}</p>
                   </div>
                 </li>
               );
@@ -96,7 +106,6 @@ export function Room() {
         <div className="chat-room-message-container">
           <textarea
             ref={messageInputRef}
-            // contentEditable="true"
             onChange={handleOnChange}
             value={message}
             id="chat-message-input"
@@ -106,22 +115,21 @@ export function Room() {
           />
         </div>
         <div className="chat-room-send-button-container">
-          <div className="send-message-button-container" >
-            <input
-              type="button"
-              value="Send"
-              id="chat-message-submit"
-              title="Press Ctrl+Enter to send"
-              onClick={() => {
-                handleSendMessage(message);
-                setMessage('');
-                messageInputRef.current.focus();
-              }}
-              disabled={!isConnected}
-              className="send-message-button"
-            />
+          <button
+            type="button"
+            value=""
+            id="chat-message-submit"
+            title="Press Ctrl+Enter to send"
+            onClick={() => {
+              handleSendMessage(message);
+              setMessage('');
+              messageInputRef.current.focus();
+            }}
+            disabled={!isConnected}
+            className="send-message-button"
+          >
             <FiSend className="send-icon" size={20} />
-          </div>
+          </button>
         </div>
       </div>
     </div>
